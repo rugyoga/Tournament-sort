37263cb8c693c390763466dd52acf104afcccfbd
Guy Argo <guy.argo@gmail.com>
2012-03-02 17:26:54 -0800

    Fixed formatting

--- java/generics/Tournament.java
+++ java/generics/Tournament.java
@@ -6,67 +6,67 @@ class Tournament<T>
 {
   Match<T> tourney;
   Comparator<T> comparator;
-	
+
   Tournament( Comparator<T> comparator, T[] v )
   {
-	this.comparator = comparator;
-	this.tourney    = knockout( v, 0, v.length-1 );
+    this.comparator = comparator;
+    this.tourney    = knockout( v, 0, v.length-1 );
   }
-  
+
   public T pop()
   {
-	T result = tourney.winner;
-	tourney = tourney.isPlayer() ? null : tourney.rebuild( comparator );
-	return result;
+    T result = tourney.winner;
+    tourney = tourney.isPlayer() ? null : tourney.rebuild( comparator );
+    return result;
   }
 
   public static <T> void sort( T[] v, Comparator<T>  comparator )
   {
     Tournament<T> tourney = new Tournament<T>( comparator, v );
     for (int i = 0; i < v.length; i++)
-		v[i] = tourney.pop();
+      v[i] = tourney.pop();
   }
 
   private Match<T> knockout( T[] v, int i, int k )
   {
-	if (i == k) return new Match<T>( v[i] );
+    if (i == k) return new Match<T>( v[i] );
     int j = (i+k)/2;
-	return new Match<T>( comparator, knockout( v, i, j ), knockout( v, j+1, k ) );
+    return new Match<T>( comparator, knockout( v, i, j ), knockout( v, j+1, k ) );
   }
 
-static class Match<Player>
-{
-  Match<Player> losers, winners;
-  Player winner;
-
-  Match( Player player )
+  static class Match<Player>
   {
-	 this.winner  = player;
-  }
+    Match<Player> losers, winners;
+    Player winner;
 
-  Match( Comparator<Player> comparator, Match<Player> a, Match<Player> b )
-  {
-	boolean result  = comparator.compare( a.winner, b.winner ) < 0;
-    this.losers     = result ? b : a;
-	this.winners    = result ? a : b;
-	this.winner     = result ? a.winner : b.winner;
-  }
+    Match( Player player )
+    {
+      this.winner = player;
+    }
 
-  public boolean isPlayer(){ return losers == null || winners == null; }
+    Match( Comparator<Player> comparator, Match<Player> a, Match<Player> b )
+    {
+      boolean result  = comparator.compare( a.winner, b.winner ) < 0;
+      this.losers     = result ? b : a;
+      this.winners    = result ? a : b;
+      this.winner     = result ? a.winner : b.winner;
+    }
 
-  public String toString()
-  {
-	return isPlayer() ? "([" + winner + "])" : "([" + winner + "]" + winners + " " + losers + ")";
-  }
+    public boolean isPlayer(){ return losers == null || winners == null; }
 
-  public Match<Player> rebuild( Comparator<Player> comparator )
-  {
-    return winners.isPlayer() ? losers : new Match<Player>( comparator, losers, winners.rebuild( comparator ) ); 
+    public String toString()
+    {
+      return isPlayer() ? "[" + winner + "]" : "(" + winner + " " + winners + " " + losers + ")";
+    }
+
+    public Match<Player> rebuild( Comparator<Player> comparator )
+    {
+      return winners.isPlayer() ? losers : new Match<Player>( comparator, losers, winners.rebuild( comparator ) ); 
+    }
   }
-}
   static Integer[] randomInts( int n )
   {
-	Random r = new Random( 12345678 );
+    Random r = new Random( 12345678 );
     Integer[] v = new Integer[n];
     for (int i = 0; i < n; i++)
       v[i] = r.nextInt( 10*n );
@@ -77,34 +77,34 @@ static class Match<Player>
   {
     long start = System.currentTimeMillis();
     action.run();
-	long finish = System.currentTimeMillis();
+    long finish = System.currentTimeMillis();
     System.out.println( description + " took " + ((double)(finish-start)/1000.0) +" secs " + compare.count + " comparisons" );	
   }
 
   static class InstrumentedCompare implements Comparator<Integer>
   {
     public int count = 0;
-	public int compare( Integer a, Integer b )
-	{
-		count++;
-		return a.compareTo( b );
-	}     	
+    public int compare( Integer a, Integer b )
+    {
+      count++;
+      return a.compareTo( b );
+    }     	
   }
 
   public static void main( String[] args )
   {
-     int n = args.length >= 1 ? Integer.parseInt( args[0] ) : 100000;
-     final InstrumentedCompare tournamentCompare = new InstrumentedCompare();
-	 final Integer[] nums = randomInts( n );
-	 time( "Tournament.sort", new Runnable(){ public void run(){ Tournament.sort( nums, tournamentCompare ); } }, tournamentCompare );
-	 final Integer[] nums2 = randomInts( n );
-     final InstrumentedCompare systemCompare = new InstrumentedCompare();
-	 time( "Array.sort", new Runnable(){ public void run(){ Arrays.sort( nums2, systemCompare ); } }, systemCompare );
-	 for (int i = 0; i < n; i++)
-	   if (nums[i].compareTo( nums2[i] ) != 0)
-	   {
-	      System.err.println( "Arrays do not match at index: " + i );
-          return;
-	   }
+    int n = args.length >= 1 ? Integer.parseInt( args[0] ) : 100000;
+    final InstrumentedCompare tournamentCompare = new InstrumentedCompare();
+    final Integer[] nums = randomInts( n );
+    time( "Tournament.sort", new Runnable(){ public void run(){ Tournament.sort( nums, tournamentCompare ); } }, tournamentCompare );
+    final Integer[] nums2 = randomInts( n );
+    final InstrumentedCompare systemCompare = new InstrumentedCompare();
+    time( "Array.sort", new Runnable(){ public void run(){ Arrays.sort( nums2, systemCompare ); } }, systemCompare );
+    for (int i = 0; i < n; i++)
+      if (nums[i].compareTo( nums2[i] ) != 0)
+      {
+        System.err.println( "Arrays do not match at index: " + i );
+        return;
+      }
   }
 }
