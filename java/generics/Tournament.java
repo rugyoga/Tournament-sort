fe7f2c0cf9056a4b4c9c99132afac2bfbd8e53e2
Guy Argo <guy.argo@gmail.com>
2019-06-02 17:16:05 -0700

    Revised layout based on feedback from CheckStyle linter

--- java/generics/Tournament.java
+++ java/generics/Tournament.java
@@ -1,158 +1,166 @@
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Random;
-import java.util.Arrays;
 import java.util.Stack;
 
-class Tournament<T>
-{
+class Tournament<T> {
   Match<T> tourney;
   Comparator<T> comparator;
   // Stack<Match<T>> stack = new Stack<Match<T>>(); required for rebuild_iterative
 
-  Tournament( Comparator<T> comparator, T[] v )
-  {
+  Tournament(Comparator<T> comparator, T[] v) {
     this.comparator = comparator;
-    this.tourney    = knockout( v, 0, v.length-1 );
+    this.tourney    = knockout(v, 0, v.length - 1);
   }
 
-  public T pop()
-  {
+  public T pop() {
     T result = tourney.winner;
-    tourney = tourney.isPlayer() ? null : tourney.rebuild_imperative( comparator );
+    tourney = tourney.isPlayer() ? null : tourney.rebuild_imperative(comparator);
     return result;
   }
 
-  public static <T> void sort( T[] v, Comparator<T>  comparator )
-  {
-    Tournament<T> tourney = new Tournament<T>( comparator, v );
-    for (int i = 0; i < v.length; i++)
+  public static <T> void sort(T[] v, Comparator<T>  comparator) {
+    Tournament<T> tourney = new Tournament<T>(comparator, v);
+    for (int i = 0; i < v.length; i++) {
       v[i] = tourney.pop();
+    }
   }
 
-  private Match<T> knockout( T[] v, int i, int k )
-  {
-    if (i == k) return new Match<T>( v[i] );
-    int j = (i+k)/2;
-    return new Match<T>( comparator, knockout( v, i, j ), knockout( v, j+1, k ) );
+  private Match<T> knockout(T[] v, int i, int k) {
+    if (i == k) {
+      return new Match<T>(v[i]);
+    }
+    int j = (i + k) / 2;
+    return new Match<T>(comparator, knockout(v, i, j), knockout(v, j + 1, k));
   }
 
-  static class Match<Player>
-  {
-    Match<Player> losers, winners;
-    Player winner;
+  static class Match<PlayerT> {
+    Match<PlayerT> losers;
+    Match<PlayerT>  winners;
+    PlayerT winner;
 
-    Match( Player player )
-    {
-      this.winner = player;
+    Match(PlayerT playerT) {
+      this.winner = playerT;
     }
 
-    Match( Comparator<Player> comparator, Match<Player> a, Match<Player> b )
-    {
-      boolean result  = comparator.compare( a.winner, b.winner ) < 0;
+    Match(Comparator<PlayerT> comparator, Match<PlayerT> a, Match<PlayerT> b) {
+      boolean result  = comparator.compare(a.winner, b.winner) < 0;
       this.losers     = result ? b : a;
       this.winners    = result ? a : b;
       this.winner     = result ? a.winner : b.winner;
     }
 
-    public boolean isPlayer(){ return losers == null || winners == null; }
+    public boolean isPlayer() {
+      return losers == null || winners == null;
+    }
 
-    public String toString()
-    {
+    public String toString() {
       return isPlayer() ? "[" + winner + "]" : "(" + losers + " " + winner + " " + winners + ")";
     }
 
-    public Match<Player> rebuild_functional( Comparator<Player> comparator )
-    {
-      return winners.isPlayer() ? losers : new Match<Player>( comparator, losers, winners.rebuild_functional( comparator ) ); 
+    public Match<PlayerT> rebuild_functional(Comparator<PlayerT> comparator) {
+      return winners.isPlayer()
+             ? losers
+             : new Match<PlayerT>(comparator, losers, winners.rebuild_functional(comparator));
     }
 
-    public Match<Player> rebuild_imperative( Comparator<Player> comparator )
-    {
-      if (winners.isPlayer())
+    public Match<PlayerT> rebuild_imperative(Comparator<PlayerT> comparator) {
+      if (winners.isPlayer()) {
         return losers;
-      else
-      {
-        winners = winners.rebuild_imperative( comparator );
-        if (comparator.compare( losers.winner, winners.winner ) < 0)
-        {
+      } else {
+        winners = winners.rebuild_imperative(comparator);
+        if (comparator.compare(losers.winner, winners.winner) < 0) {
           winner = losers.winner;
-          Match<Player> temp = losers;
+          Match<PlayerT> temp = losers;
           losers = winners;
           winners = temp;
-        }
-        else
+        } else {
           winner = winners.winner;
+        }
         return this;
       }
     }
 
-    public Match<Player> rebuild_iterative( Comparator<Player> comparator, Stack<Match<Player>> stack )
-    {
-      if (winners.isPlayer()) return losers;
-      Match<Player> root = this, tourney = this;
-      while (!tourney.winners.isPlayer())
-      {
-        stack.push( tourney );
+    public Match<PlayerT> rebuild_iterative(
+        Comparator<PlayerT> comparator,
+        Stack<Match<PlayerT>> stack) {
+      if (winners.isPlayer()) {
+        return losers;
+      }
+      final Match<PlayerT> root = this;
+      Match<PlayerT> tourney = this;
+      while (!tourney.winners.isPlayer()) {
+        stack.push(tourney);
         tourney = tourney.winners;
       }
       stack.peek().winners = tourney.losers;
-      while (!stack.isEmpty())
-      {
-        Match<Player> t = stack.pop();
-        if (comparator.compare( t.losers.winner, t.winners.winner ) < 0)
-        {
+      while (!stack.isEmpty()) {
+        Match<PlayerT> t = stack.pop();
+        if (comparator.compare(t.losers.winner, t.winners.winner) < 0) {
           t.winner = t.losers.winner;
-          Match<Player> temp = t.losers;
+          Match<PlayerT> temp = t.losers;
           t.losers = t.winners;
           t.winners = temp;
-        }
-        else
+        } else {
           t.winner = t.winners.winner;
+        }
       }
       return root;
     }
   }
-  static Integer[] randomInts( int n )
-  {
-    Random r = new Random( 12345678 );
+
+  static Integer[] randomInts(int n) {
+    Random r = new Random(12345678);
     Integer[] v = new Integer[n];
-    for (int i = 0; i < n; i++)
-      v[i] = r.nextInt( 10*n );
+    for (int i = 0; i < n; i++) {
+      v[i] = r.nextInt(10 * n);
+    }
     return v;
   }
 
-  static void time( String description, Runnable action, InstrumentedCompare compare )
-  {
+  static void time(String description, Runnable action, InstrumentedCompare compare) {
     long start = System.currentTimeMillis();
     action.run();
     long finish = System.currentTimeMillis();
-    System.out.println( description + " took " + ((double)(finish-start)/1000.0) +" secs " + compare.count + " comparisons" );	
+    System.out.println(description + " took "
+                       + ((double)(finish - start) / 1000.0) + " secs "
+                       + compare.count + " comparisons");
   }
 
-  static class InstrumentedCompare implements Comparator<Integer>
-  {
+  static class InstrumentedCompare implements Comparator<Integer> {
     public int count = 0;
-    public int compare( Integer a, Integer b )
-    {
+
+    public int compare(Integer a, Integer b) {
       count++;
-      return a.compareTo( b );
-    }     	
+      return a.compareTo(b);
+    }
   }
 
-  public static void main( String[] args )
-  {
-    int n = args.length >= 1 ? Integer.parseInt( args[0] ) : 100000;
+  public static void main(String[] args) {
+    int n = args.length >= 1 ? Integer.parseInt(args[0]) : 100000;
     final InstrumentedCompare tournamentCompare = new InstrumentedCompare();
-    final Integer[] nums = randomInts( n );
-    time( "Tournament.sort", new Runnable(){ public void run(){ Tournament.sort( nums, tournamentCompare ); } }, tournamentCompare );
-    final Integer[] nums2 = randomInts( n );
+    final Integer[] nums = randomInts(n);
+    time("Tournament.sort",
+         new Runnable() {
+          public void run() {
+            Tournament.sort(nums, tournamentCompare);
+          }
+          },
+         tournamentCompare);
+    final Integer[] nums2 = randomInts(n);
     final InstrumentedCompare systemCompare = new InstrumentedCompare();
-    time( "Array.sort", new Runnable(){ public void run(){ Arrays.sort( nums2, systemCompare ); } }, systemCompare );
-    for (int i = 0; i < n; i++)
-      if (nums[i].compareTo( nums2[i] ) != 0)
-      {
-        System.err.println( "Arrays do not match at index: " + i );
+    time("Array.sort",
+         new Runnable() {
+          public void run() {
+            Arrays.sort(nums2, systemCompare);
+          }
+          },
+         systemCompare);
+    for (int i = 0; i < n; i++) {
+      if (nums[i].compareTo(nums2[i]) != 0) {
+        System.err.println("Arrays do not match at index: " + i);
         return;
       }
+    }
   }
 }
